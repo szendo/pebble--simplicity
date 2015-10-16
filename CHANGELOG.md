4dc231ec38bfc14ac2c658487d2d6c80c13365d8
szendo <pebble-dev@szendo.xyz>
2015-10-16 07:31:00 +0000

    Unreleased version 1.4x

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## [Unreleased]
+
+Added day of the week above the date.
+
+Preparations to support the Pebble Time Round.
+
 ## [1.1] - 2015-06-14
 
 Added battery status and Bluetooth connection indicators.
--- src/simplicity.c
+++ src/simplicity.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
 
 static Window *s_main_window;
+static TextLayer *s_dow_layer;
 static TextLayer *s_date_layer;
 static Layer *s_line_layer, *s_time_layer;
 static Layer *s_status_layer;
@@ -41,32 +42,40 @@ static void time_layer_update_callback(Layer *layer, GContext* ctx) {
 }
 
 static void status_layer_update_callback(Layer *layer, GContext* ctx) {
+  int batt_x_off = PBL_IF_RECT_ELSE(24, 12);
   if (!s_connected) {
     graphics_draw_bitmap_in_rect(ctx, s_conn_bitmap, GRect(0, 0, 24, 24));
+#ifdef PBL_ROUND
+    batt_x_off = 24;
+#endif
   }
   if (s_battery.is_charging) {
-    graphics_draw_bitmap_in_rect(ctx, s_batt_charge_bitmap, GRect(24, 0, 24, 24));
+    graphics_draw_bitmap_in_rect(ctx, s_batt_charge_bitmap, GRect(batt_x_off, 0, 24, 24));
   } else {
     uint16_t charge = s_battery.charge_percent;
-    graphics_draw_bitmap_in_rect(ctx, s_batt_bitmap, GRect(24, 0, 24, 24));
+    graphics_draw_bitmap_in_rect(ctx, s_batt_bitmap, GRect(batt_x_off, 0, 24, 24));
     graphics_context_set_fill_color(ctx, charge <= 20 ? GColorRed 
                                        : charge <= 30 ? GColorOrange
                                        : charge <= 40 ? GColorChromeYellow
                                        : charge <= 50 ? GColorYellow
                                        : GColorWhite);
     uint16_t batt_fill = 14 * charge / 100;
-    graphics_fill_rect(ctx, GRect(33, 20 - batt_fill, 6, batt_fill), 0, GCornerNone);
+    graphics_fill_rect(ctx, GRect(batt_x_off + 9, 20, 6, -batt_fill), 0, GCornerNone);
   }
 }
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   static char s_date_text[] = "Xxxxxxxxx 00";
+  static char s_dow_text[] = "Xxxxxxxxx 00";
   
   memcpy(&curr_time, tick_time, sizeof(struct tm));
   layer_mark_dirty(s_time_layer);
 
   strftime(s_date_text, sizeof(s_date_text), "%B %e", tick_time);
   text_layer_set_text(s_date_layer, s_date_text);
+
+  strftime(s_dow_text, sizeof(s_dow_text), "%A", tick_time);
+  text_layer_set_text(s_dow_layer, s_dow_text);
 }
 
 static void handle_connection_change(bool connected) {
@@ -82,21 +91,30 @@ static void handle_battery_state_change(BatteryChargeState battery) {
 static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
 
-  s_date_layer = text_layer_create(GRect(8, 62, 136, 100));
+  int x_off = PBL_IF_RECT_ELSE(0, 18);
+  int y_off = PBL_IF_RECT_ELSE(0, 6);
+  s_date_layer = text_layer_create(GRect(8 + x_off, 62 + y_off, 136, 100));
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 
-  s_time_layer = layer_create(GRect(7, 105, 125, 40));
+  s_dow_layer = text_layer_create(GRect(8 + x_off, 34 + y_off, 136, 100));
+  text_layer_set_text_color(s_dow_layer, GColorWhite);
+  text_layer_set_background_color(s_dow_layer, GColorClear);
+  text_layer_set_font(s_dow_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+  layer_add_child(window_layer, text_layer_get_layer(s_dow_layer));
+
+  s_time_layer = layer_create(GRect(7 + x_off, 105 + y_off, 125, 40));
   layer_set_update_proc(s_time_layer, time_layer_update_callback);
   layer_add_child(window_layer, s_time_layer);
 
-  s_line_layer = layer_create(GRect(8, 97, 139, 2));
+  int w_off = PBL_IF_RECT_ELSE(0, 15);
+  s_line_layer = layer_create(GRect(8 + x_off, 97 + y_off, 139 + w_off, 2));
   layer_set_update_proc(s_line_layer, line_layer_update_callback);
   layer_add_child(window_layer, s_line_layer);
 
-  s_status_layer = layer_create(GRect(94, 2, 48, 24));
+  s_status_layer = layer_create(GRect(PBL_IF_RECT_ELSE(94, 66), 2, 48, 24));
   layer_set_update_proc(s_status_layer, status_layer_update_callback);
   layer_add_child(window_layer, s_status_layer);
 }
